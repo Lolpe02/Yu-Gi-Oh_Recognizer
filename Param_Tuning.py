96c01a638c9f458aaea9dd1676d3316f10d975af
Lolpe02 <pellegrino.2007948@studenti.uniroma1.it>
2023-08-27 22:19:33 +0200

    - canny hyperparameter optimizer - new corner finder method

---
+++ Param_Tuning.py
@@ -0,0 +1,82 @@
+import cv2
+import numpy as np
+
+
+def find_corner_points(contour,method=1):
+    if len(contour) < 4:
+        print("not enough points")
+        return None, None, None, None
+
+    if method :
+        # Find the point with the lowest sum of coordinates (top-left corner)
+        top_left = tuple(contour[np.argmin(contour.sum(axis=2))])
+
+        # Find the point with the highest difference of coordinates (top-right corner)
+        top_right = tuple(contour[np.argmax(np.diff(contour, axis=2))])
+
+        # Find the point with the highest sum of coordinates (bottom-right corner)
+        bottom_right = tuple(contour[np.argmax(contour.sum(axis=2))])
+
+        # Find the point with the lowest difference of coordinates (bottom-left corner)
+        bottom_left = tuple(contour[np.argmin(np.diff(contour, axis=2))])
+
+        return top_left, top_right, bottom_right, bottom_left
+    elif not method:
+        top_left = tuple(contour[contour[:, :, 0].argmin()][0])
+
+        top_right = tuple(contour[contour[:, :, 0].argmax()][0])
+
+        bottom_right = tuple(contour[contour[:, :, 1].argmin()][0])
+
+        bottom_left = tuple(contour[contour[:, :, 1].argmax()][0])
+
+        return top_left, top_right, bottom_right, bottom_left
+
+def is_rectangle(contour):
+    # Calculate contour properties
+    perimeter = cv2.arcLength(contour, True)
+    vertices = cv2.approxPolyDP(contour, 0.03 * perimeter, True)
+    x, y, width, height = cv2.boundingRect(vertices)
+    card_ratio = 590 / 860
+    # Calculate aspect ratio of the bounding rectangle
+    aspect_ratio = width / height
+
+    # Check if the contour has 4 vertices and aspect ratio close to 1
+    return len(vertices) == 4 and aspect_ratio >= card_ratio - 0.05 and aspect_ratio <= card_ratio + 0.05
+
+
+def find_optimal_canny_threshold(im, max_edge_count, threshold_range=(0, 500), step=30):
+    best_threshold1 = None
+    best_threshold2 = None
+    l = []
+    image = cv2.GaussianBlur(cv2.imread(im), (3, 3), 0)
+    for threshold1 in range(threshold_range[0], threshold_range[1] - 1, step):
+        for threshold2 in range(threshold1+ step, threshold_range[1], step):
+            edges = cv2.Canny(image, threshold1, threshold2, None, 3, True)
+            edges = cv2.rectangle(edges, (50, 50), (600, 300), (255, 255, 255), -1, 8)
+            edges = cv2.putText(edges, "t1= " + str(threshold1) + ", t2= " + str(threshold2) + "\n press q to quit\npress enter to fuck", (80, 90),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1.6, (0, 0, 0), 6)
+            cv2.imshow("edges", cv2.resize(edges, None, fx=0.45, fy=0.45, interpolation=cv2.INTER_CUBIC))
+
+            key = cv2.waitKey(0)
+            # wait for user input
+            if key == 13:
+                contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                best_threshold1 = threshold1
+                best_threshold2 = threshold2
+                l.append((best_threshold1, best_threshold2))
+                for c in contours:
+                    if cv2.contourArea(c) > 350 and is_rectangle(c):  #
+                        cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+                cv2.imshow("contours", cv2.resize(image, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
+                continue
+            elif key == ord("q"):
+                return l
+            else:
+                continue
+
+    return l
+
+
+if __name__ == '__main__':
+    print(find_optimal_canny_threshold('carta11.jpeg', 4))
--- main.py
+++ main.py
@@ -2,72 +2,85 @@ import cv2
 import pytesseract as pt
 from PIL import Image
 import numpy as np
-import Perfect_Rotation as pr
+import Param_Tuning as pmt
+
 # import nltk
 
-def is_rectangle(contour):
-    # Calculate contour properties
-    perimeter = cv2.arcLength(contour, True)
-    vertices = cv2.approxPolyDP(contour, 0.01 * perimeter, True)
-    # print(vertices)
-    x, y, width, height = cv2.boundingRect(vertices)
-    card_ratio = 590 / 860
-    # Calculate aspect ratio of the bounding rectangle
-    aspect_ratio = width / height
-
-    # Check if the contour has 4 vertices and aspect ratio close to 1
-    return len(vertices) == 4 and aspect_ratio >= card_ratio - 0.3 and aspect_ratio <= card_ratio + 0.2
 
 pt.pytesseract.tesseract_cmd = r'D:\Program Files\TesseractOCR\tesseract.exe'
-print(pt.get_languages())
-namelist =[]
-kernel = np.ones((3,3), np.uint8)
-img = cv2.imread('carta3.jpeg')
-#TODO: try HSV thresholding Canny edge detection
-img = cv2.resize(img, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
-blur_img = cv2.GaussianBlur(img, (3,3),0)
+# print(pt.get_languages())
+namelist = []
+kernel = np.ones((3, 3), np.uint8)
+img = cv2.imread('carta11.jpeg')
+# TODO: try HSV thresholding Canny edge detection
 
-edges = cv2.Canny(blur_img, 100, 300)
-# edges = cv2.dilate(edges, kernel, iterations= 1)
-cv2.imshow("edges", edges)
-contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# img = cv2.resize(img, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+blur_img = cv2.GaussianBlur(img, (3, 3), 0)
+ret, thresh = cv2.threshold(cv2.cvtColor(blur_img, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+edges1 = cv2.Canny(blur_img, 100, 300)  # , None, 3, True #(140, 481, False)
+# edges2 = cv2.Canny(blur_img, 300, 500)
+cv2.imshow("edges1", cv2.resize(edges1, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
+# cv2.imshow("edges2", cv2.resize(edges2, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
+# cv2.waitKey(0)
+contours, hierarchy = cv2.findContours(edges1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+if contours is None:
+    print("no contours found")
+    exit()
 im2 = img.copy()
 count = 0
+failed = 0
 # Define the dimensions of the output image (width, height)
-output_width = 590 //2
-output_height = 860 //2
+output_width = 590
+output_height = 860
 # top_left, top_right, bottom_right, bottom_left
-output_corners = np.float32([[0, 0], [output_width - 1, 0], [output_width - 1, output_height - 1], [0, output_height - 1]])
-for c in contours:
-    if cv2.contourArea(c) > 300 and is_rectangle(c):
+output_corners = np.float32([[0, 0], [output_width, 0], [output_width, output_height], [0, output_height]])
 
+for c in contours:
+    if cv2.contourArea(c) > 350 and pmt.is_rectangle(c):  #
+        print("found a rectangle")
         cv2.drawContours(im2, [c], -1, (0, 255, 0), 2)
         HL, LR, alpha = cv2.minAreaRect(c)
-        # print(alpha)
         # always clockwise starting from the top left
-        box = cv2.boxPoints((HL, LR, alpha))
-        box = np.array(box, dtype=np.float32)
+        tl, tr, br, bl = pmt.find_corner_points(c)
+        box = np.array([tl, tr, br, bl], dtype=np.float32).reshape(4, 2)
+        print(box.shape)
         a = 0
         for p in box:
             # print(p)
-            cv2.circle(im2, p.astype(int), 5,(a *63,a * 63,a * 63), -1)
+            cv2.circle(im2, p.astype(int), 5, (a * 63, a * 63, a * 63), -1)
             a += 1
-        box
-        #TODO: check if the box is clockwise or not and trsform as needed
-        if box[1,0] - box[0,0] > box[2,1] - box[1,1]:
-            9 #box = np.concatenate(box[-1] + box[:-1])
 
-        count+=1
+        # TODO: check if the box is clockwise or not and trasform as needed
+        if box[1,0] - box[0,0] > box[2,1] - box[1,1]:
+            print("ordering")
+            box = np.array([box[3], box[0], box[1], box[2]], dtype=np.float32)
+
+        count += 1
         transformation_matrix = cv2.getPerspectiveTransform(box, output_corners)
         warped_image = cv2.warpPerspective(img.copy(), transformation_matrix, (output_width, output_height))
+        x, y = warped_image.shape[:2]
+        # name = warped_image[x//16:x//7,y//12:y*33//40]
+        name = warped_image[:x // 7, :]
+        # sharpening with blur and kernel
+        blurred = cv2.GaussianBlur(name, (3, 3), 0)
+        name = cv2.addWeighted(name, 1.5, blurred, -0.5, 0)
 
-        text = pt.image_to_string(warped_image, lang='ita')
+        # kernel = np.array([[1, -1, -1], [0, 5, -1], [1, -1,-1]])
+        # warped_image = cv2.filter2D(warped_image, -1, kernel)
+        # resize
+        # warped_image = cv2.resize(warped_image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+        text = pt.image_to_string(name, lang='ita')
         print(text)
-        cv2.imshow(f"contours of {count}",im2)
+        cv2.imshow(f"name of {count}", name)
 
+    else:
+        # cv2.drawContours(im2, [c], -1, (0,0,255), 8)
+        # print("smt wrongobongo")
+        failed += 1
 
-
-# cv2.imshow("contours", im2)
+im2 = cv2.resize(im2, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+cv2.imshow("contours", im2)
 # im2 = img.copy()
 # lines = cv2.HoughLines(edges, 1, np.pi/180, 100)
 # for line in lines:
@@ -119,7 +132,6 @@ for c in contours:
 # # sobel = cv2.addWeighted(sobelx,0.5,sobely,0.5,0)
 
 
-
 #
 # Define the coordinates of the corresponding corners in the output image
 # output_corners = np.float32([[0, 0], [output_width - 1, 0], [output_width - 1, output_height - 1], [0, output_height - 1]])
@@ -132,7 +144,7 @@ for c in contours:
 #
 #
 # #--- convert the image to HSV color space ---
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+# hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 # # find hsv of points
 # # for point in [[191, 319], [128, 666], [360, 620], [401, 442]]:
 # #     print(hsv[point[1],point[0]]) #why not [point[0],point[1]]? because opencv is BGR not RGB
