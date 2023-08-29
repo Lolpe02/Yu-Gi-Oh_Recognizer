b49bddf7eedbb7b0e9958d7c00731183b74dacf6
Lolpe02 <pellegrino.2007948@studenti.uniroma1.it>
2023-08-29 19:26:37 +0200

    don't give up skeleton :) joy ahead

--- Param_Tuning.py
+++ Param_Tuning.py
@@ -5,22 +5,23 @@ import numpy as np
 def find_corner_points(contour,method=1):
     if len(contour) < 4:
         print("not enough points")
-        return None, None, None, None
+        return None
 
     if method :
         # Find the point with the lowest sum of coordinates (top-left corner)
         top_left = tuple(contour[np.argmin(contour.sum(axis=2))])
 
         # Find the point with the highest difference of coordinates (top-right corner)
-        top_right = tuple(contour[np.argmax(np.diff(contour, axis=2))])
+        top_right = tuple(contour[np.argmin(np.diff(contour, axis=2))])
 
         # Find the point with the highest sum of coordinates (bottom-right corner)
         bottom_right = tuple(contour[np.argmax(contour.sum(axis=2))])
 
         # Find the point with the lowest difference of coordinates (bottom-left corner)
-        bottom_left = tuple(contour[np.argmin(np.diff(contour, axis=2))])
+        bottom_left = tuple(contour[np.argmax(np.diff(contour, axis=2))])
 
-        return top_left, top_right, bottom_right, bottom_left
+        return np.array([top_left, top_right, bottom_right, bottom_left], dtype=np.int32).reshape(4, 2)
+
     elif not method:
         top_left = tuple(contour[contour[:, :, 0].argmin()][0])
 
@@ -30,40 +31,62 @@ def find_corner_points(contour,method=1):
 
         bottom_left = tuple(contour[contour[:, :, 1].argmax()][0])
 
-        return top_left, top_right, bottom_right, bottom_left
+        return np.array([top_left, top_right, bottom_right, bottom_left], dtype=np.int32).reshape(4, 2)
 
 def is_rectangle(contour):
     # Calculate contour properties
     perimeter = cv2.arcLength(contour, True)
-    vertices = cv2.approxPolyDP(contour, 0.1 * perimeter, True)
-    x, y, width, height = cv2.boundingRect(vertices)
+    vertices = cv2.approxPolyDP(contour, 0.2 * perimeter, True)
+
+
+    if len(vertices) != 4:
+        return False
+    print(vertices)
+    vertices.reshape(4, 2) #tl, tr, br, bl
+
+    # calculate distance between two points
+    height1, height2 = cv2.norm(vertices[::2,:], cv2.NORM_L2), cv2.norm(vertices[1::2,:], cv2.NORM_L2)
+    width1, width2 = cv2.norm(vertices[:2,:], cv2.NORM_L2), cv2.norm(vertices[3:,:], cv2.NORM_L2)
+    print(width1,width2, height1,height2)
     card_ratio = 590 / 860
     # Calculate aspect ratio of the bounding rectangle
-    aspect_ratio = width / height
-
+    aspect_ratio = width1 / height1
     # Check if the contour has 4 vertices and aspect ratio close to 1
-    return len(vertices) == 4 and aspect_ratio >= card_ratio - 0.3 and aspect_ratio <= card_ratio + 0.3
+    print("done")
+    return aspect_ratio >= card_ratio - 0.2 and aspect_ratio <= card_ratio + 0.2
 
 
-def find_optimal_canny_threshold(im ,threshold_range=(0, 500), step=30):
+def find_optimal_canny_threshold(im , test = None , threshold_range=(0, 511), step=30):
     best_threshold1 = None
     best_threshold2 = None
     l = []
+    kernel = np.ones((2,2), np.uint8)
     im = cv2.imread(im)
+    image = cv2.cvtColor(im, cv2.COLOR_HSV2BGR)
     # image = cv2.GaussianBlur(cv2.imread(im), (3, 3), 0)
-    image = cv2.bilateralFilter(im, 5, 10, 10)
+    # image = cv2.bilateralFilter(image, 5, 10, 10)
+    if test:
+        step = 10
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        ret, thresh_H = cv2.threshold(hsv[:, :, 0], 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        ret, thresh_S = cv2.threshold(hsv[:, :, 1], 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        ret, thresh_V = cv2.threshold(hsv[:, :, 2], 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        t = thresh_V - thresh_H - thresh_S
+        image = cv2.bitwise_not(t)
+
     for threshold1 in range(threshold_range[0], threshold_range[1] - 1, step):
-        for threshold2 in range(threshold1+ step, threshold_range[1], step):
+        for threshold2 in range(threshold_range[0], threshold_range[1], step*10):
             edges = cv2.Canny(image, threshold1, threshold2, None, 3, True)
+            edges = cv2.dilate(edges, kernel, iterations=4)
             sedges = cv2.rectangle(edges, (50, 50), (1100, 150), (255, 255, 255), -1, 8)
             sedges = cv2.putText(sedges, f"t1= {threshold1} , t2= {threshold2}", (80, 90),
                                 cv2.FONT_HERSHEY_SIMPLEX, 1.6, (0, 0, 0), 6)
             sedges = cv2.putText(sedges, f"q = quit, enter = try, any key = next",
                                 (80, 130),
                                 cv2.FONT_HERSHEY_SIMPLEX, 1.6, (0, 0, 0), 6)
-            cv2.imshow("edges", cv2.resize(sedges, None, fx=0.45, fy=0.45, interpolation=cv2.INTER_CUBIC))
+            cv2.imshow("cannyHSV" if test else "cannyOG", cv2.resize(sedges, None, fx=0.45, fy=0.45, interpolation=cv2.INTER_CUBIC))
 
-            key = cv2.waitKey(0)
+            key = cv2.waitKey(0) & 0xFF
             # wait for user input
             if key == 13:
                 contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -71,10 +94,10 @@ def find_optimal_canny_threshold(im ,threshold_range=(0, 500), step=30):
                 best_threshold2 = threshold2
                 l.append((best_threshold1, best_threshold2))
                 for c in contours:
-                    if cv2.contourArea(c) > 450 :  #and is_rectangle(c)
-                        new =  cv2.drawContours(image.copy(), contours, -1, (0, 255, 0), 2)
-                cv2.imshow("contours", cv2.resize(new, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
-                cv2.waitKey(5000)
+                    if cv2.contourArea(c) > 450 and is_rectangle(c) :  #
+                        image =  cv2.drawContours(image, contours, -1, (0, 255, 0), 4)
+                cv2.imshow("contours", cv2.resize(image, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
+
             elif key == ord("q"):
                 return l
             else:
@@ -82,54 +105,82 @@ def find_optimal_canny_threshold(im ,threshold_range=(0, 500), step=30):
                 continue
 
     return l
-def find_optimal_hsv_threshold(im , step=10):# [(20, 170), (20, 180), (200, 70), (200, 150), (210, 120), (220, 80), (230, 70), (230, 80)]
+def find_optimal_hsv_threshold(im ,testlist = None , step=15):# []
     kernel = np.ones((1, 1), np.uint8)
     l = []
     im = cv2.imread(im)
+    area1 = np.multiply(*im.shape[:2]) - 1
     # im = cv2.GaussianBlur(im, (3, 3), 0)
     im = cv2.bilateralFilter(im, 5, 10, 10)
     hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
-    for threshold1 in range(0, 255 , step):
-        for threshold2 in range(0,255, step):
-
-            ret, thresh_H = cv2.threshold(hsv[:, :, 0], threshold1, 255, cv2.THRESH_BINARY  )
-            ret, thresh_S = cv2.threshold(hsv[:, :, 1], threshold2, 255, cv2.THRESH_BINARY )
-
+    if testlist:
+        for test in testlist:
+            ret, thresh_H = cv2.threshold(hsv[:, :, 0], test[0], 255, cv2.THRESH_BINARY)
+            ret, thresh_S = cv2.threshold(hsv[:, :, 1], test[1], 255, cv2.THRESH_BINARY)
+            ret, thresh_V = cv2.threshold(hsv[:, :, 2], test[3], 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
             # --- add the result of the above two ---
-
-            sumt = thresh_H + thresh_S
-            neg = cv2.bitwise_not(sumt)
+            t = thresh_V- thresh_H + thresh_S
+            neg = cv2.bitwise_not(t)
             # --- some morphology operation to clear unwanted spots ---
-            dilation = cv2.dilate(sumt, kernel, iterations=10)
-            cv2.imshow('thresh', cv2.resize(sumt, None, fx=0.45, fy=0.45, interpolation=cv2.INTER_CUBIC))
-            # edges = cv2.Canny(dilation, 120, 400)
-            sedges = cv2.rectangle(dilation, (50, 50), (1100, 150), (255, 255, 255), -1, 8)
-            sedges = cv2.putText(sedges, f"t1= {threshold1} , t2= {threshold2}", (80, 90),
-                                cv2.FONT_HERSHEY_SIMPLEX, 1.6, (0, 0, 0), 6)
-            sedges = cv2.putText(sedges, f"q = quit, enter = try, any key = next",
-                                (80, 130),
-                                cv2.FONT_HERSHEY_SIMPLEX, 1.6, (0, 0, 0), 6)
-            cv2.imshow("edges", cv2.resize(sedges, None, fx=0.45, fy=0.45, interpolation=cv2.INTER_CUBIC))
+            dilation = cv2.dilate(neg, kernel, iterations=5)
+            cv2.imshow(f"added {test}", cv2.resize(t, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
+            cv2.imshow(f"neg {test}" , cv2.resize(dilation, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
+        cv2.waitKey(0)
+        return
+    for threshold1 in range(0, 255 , step):
+        for threshold2 in range(0, 255, step):
+            for threshold3 in range(0, 255, step):
+                ret, thresh_H = cv2.threshold(hsv[:, :, 0], threshold1, 255, cv2.THRESH_BINARY)
+                ret, thresh_S = cv2.threshold(hsv[:, :, 1], threshold2, 255, cv2.THRESH_BINARY)
+                ret, thresh_V = cv2.threshold(hsv[:, :, 2], threshold3, 255, cv2.THRESH_BINARY)
+                # --- add the result of the above two ---
+                t = thresh_V + thresh_H + thresh_S
+                neg = cv2.bitwise_not(t)
+                # --- some morphology operation to clear unwanted spots ---
+                dilation = cv2.dilate(neg, kernel, iterations=5)
+                cv2.imshow("added", cv2.resize(t, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
+                cv2.imshow("neg", cv2.resize(dilation, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
+                sedges = cv2.rectangle(dilation + t, (50, 50), (1100, 150), (255, 255, 255), -1, 8)
+                sedges = cv2.putText(sedges, f"t1={threshold1} , t2={threshold2}, t3={threshold3}", (80, 90),
+                                     cv2.FONT_HERSHEY_SIMPLEX, 1.6, (0, 0, 0), 6)
+                sedges = cv2.putText(sedges, f"q = quit, enter = try, any key = next",
+                                     (80, 130),
+                                     cv2.FONT_HERSHEY_SIMPLEX, 1.6, (0, 0, 0), 6)
+                cv2.imshow("edges", cv2.resize(sedges, None, fx=0.45, fy=0.45, interpolation=cv2.INTER_CUBIC))
 
-            key = cv2.waitKey(0)
-            # wait for user input
-            if key == 13:
-                contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                best_threshold1 = threshold1
-                best_threshold2 = threshold2
-                l.append((best_threshold1, best_threshold2))
-                for c in contours:
-                    if cv2.contourArea(c) > 450 and is_rectangle(c) :  #
-                        im =  cv2.drawContours(im.copy(), contours, -1, (0, 255, 0), 2)
-                cv2.imshow("contours", cv2.resize(im, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
-                cv2.waitKey(5000)
-            elif key == ord("q"):
-                return l
-            else:
+                key = cv2.waitKey(0)
+                # wait for user input
+                if key == 13:
+                    contours1, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                    contours2, hierarchy = cv2.findContours(t, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                    best_threshold1 = threshold1
+                    best_threshold2 = threshold2
+                    l.append((best_threshold1, best_threshold2))
+                    for c in contours1:
+                        area = cv2.contourArea(c)
+                        if area > 450 and area < area1 and is_rectangle(c):  #
+                            im = cv2.drawContours(im.copy(), contours1, -1, (0, 255, 0), 2)
+                    for c in contours2:
+                        area = cv2.contourArea(c)
+                        if area > 450 and area < area1 and is_rectangle(c):  #
+                            im = cv2.drawContours(im, contours2, -1, (0, 0, 255), 2)
+                    cv2.imshow("contours", cv2.resize(im, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
+                elif key == ord("q"):
+                    return l
+                elif key == ord("d"):
+                    l.pop()
+                else:
 
-                continue
+                    continue
+
 
     return l
 
+carta5 =[(20, 130), (20, 250), (30, 250), (40, 250), (90, 120), (120, 60), (120, 120), (130, 60), (200, 70), (200, 120), (220, 70), (220, 100), (250, 70)]
+carta11 = [ (40, 30), (60, 20), (190, 10)]
+
 if __name__ == '__main__':
-    print(find_optimal_hsv_threshold('carta11.jpeg', ))
+    print(find_optimal_canny_threshold('stacked.jpeg', 0,step=1 ))
+    # s = set([(20, 160), (20, 240), (30, 170), (30, 250), (50, 190), (50, 200), (50, 210), (60, 210), (60, 240), (60, 250), (80, 190), (150, 110), (160, 70), (230, 90)(70, 50), (100, 30), (130, 20), (150, 10), (190, 10), (190, 20), (200, 20), (210, 20), (210, 40), (220, 10), (220, 20),(20, 170), (20, 180), (200, 70), (200, 150), (210, 120), (220, 80), (230, 70), (230, 80)])
+    # print(s)
+
--- main.py
+++ main.py
@@ -10,29 +10,37 @@ import Param_Tuning as pmt
 pt.pytesseract.tesseract_cmd = r'D:\Program Files\TesseractOCR\tesseract.exe'
 # print(pt.get_languages())
 namelist = []
-kernel = np.ones((5, 5), np.uint8)
-img = cv2.imread('carta5.jpeg')
-# TODO: try HSV thresholding Canny edge detection
+kernel = np.ones((2,2), np.uint8)
+img = cv2.imread('carta1.jpeg')
+# print(img.shape)
 
 # img = cv2.resize(img, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
-img = cv2.GaussianBlur(img, (3, 3), 0.1)
+# img = cv2.GaussianBlur(img, (3, 3), 0.1)
 blur_img = cv2.bilateralFilter(img, 9, 10, 10)
 hsv = cv2.cvtColor(blur_img, cv2.COLOR_BGR2HSV)
 ret, thresh_H = cv2.threshold(hsv[:,:,0], 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 ret, thresh_S = cv2.threshold(hsv[:,:,1], 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+ret, thresh_V = cv2.threshold(hsv[:,:,2], 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 #--- add the result of the above two ---
-cv2.imshow('thresh', thresh_H + thresh_S)
 
-neg = cv2.bitwise_not(thresh_H + thresh_S)
+t = -thresh_V - thresh_H - thresh_S
+neg = cv2.bitwise_not(t)
+# cv2.imshow('threshsum', cv2.resize(neg, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
+
 #--- some morphology operation to clear unwanted spots ---
-dilation = cv2.dilate(neg, kernel, iterations=1)
-edges1 = cv2.Canny(dilation, 120, 400)   #[(0, 480), (30, 480), (120, 150), (120, 450), (140, 481)]
-# edges2 = cv2.Canny(blur_img, 300, 500)g
-cv2.imshow("edges1", cv2.resize(edges1, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
-# cv2.imshow("edges2", cv2.resize(edges2, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
+dilation = cv2.dilate(neg, kernel, iterations=5)
+#add 3 axis to the 2d array
+
+stacked = np.dstack((thresh_H ,thresh_S,thresh_V))
+rgbstacked = cv2.cvtColor(stacked, cv2.COLOR_HSV2RGB)
+canny = cv2.Canny(rgbstacked, 0, 700)   #[(0, 480), (30, 480), (120, 150), (120, 450), (140, 481)]
+# edges2 = cv2.Canny(neg, 500, 600)
+new = cv2.dilate(canny, kernel, iterations=4)
+cv2.imshow("canny", cv2.resize(new, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
+# cv2.imshow("c2", cv2.resize(edges2, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
 # cv2.waitKey(0)
-contours, hierarchy = cv2.findContours(edges1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv2.findContours(new, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 # print(len(contours))
 if contours is None:
     print("no contours found")
@@ -41,33 +49,38 @@ im2 = img.copy()
 count = 0
 failed = 0
 # Define the dimensions of the output image (width, height)
-output_width = 590
-output_height = 860
+output_width, output_height = 590, 860
 # top_left, top_right, bottom_right, bottom_left
 output_corners = np.float32([[0, 0], [output_width, 0], [output_width, output_height], [0, output_height]])
 
 for c in contours:
-    if cv2.contourArea(c) > 450 and pmt.is_rectangle(c):#:
+    if cv2.contourArea(c) > 500:#: and pmt.is_rectangle(c)
         print("found a rectangle")
         cv2.drawContours(im2, [c], -1, (0, 255, 0), 2)
         # HL, LR, alpha = cv2.minAreaRect(c)
-        # always clockwise starting from the top left
-        tl, tr, br, bl = pmt.find_corner_points(c)
-        box = np.array([tl, tr, br, bl], dtype=np.float32).reshape(4, 2)
-        print(box.shape)
+        # always clockwise starting from the top left tl, tr, br, bl
+
+        # box = pmt.find_corner_points(c).astype(np.float32)
         a = 0
-        for p in box:
-            # print(p)
-            cv2.circle(im2, p.astype(int), 5, (a * 63, a * 63, a * 63), -1)
+        vertices = cv2.approxPolyDP(c, 0.3 * cv2.arcLength(c, True), True)
+        print(vertices)
+        # vertices.reshape(4, 2)
+        for p in vertices.astype(np.int32):
+            print(p)
+            im2 = cv2.circle(im2, p, 5, (a * 63, a * 63, a * 63), -1)
+            im2 = cv2.putText(im2, str(a) + str(p), p, cv2.FONT_HERSHEY_SIMPLEX, 1.6, (a * 63, a * 63, a * 63), 6)
             a += 1
 
         # TODO: check if the box is clockwise or not and trasform as needed
+
         # if box[1,0] - box[0,0] > box[2,1] - box[1,1]:
-        #     print("ordering")
+        #     print(box[1, 0] - box[0, 0], box[2, 1] - box[1, 1])
         #     box = np.array([box[3], box[0], box[1], box[2]], dtype=np.float32)
+        #     print("ordering\n"+str(box))
+
 
         count += 1
-        transformation_matrix = cv2.getPerspectiveTransform(box, output_corners)
+        transformation_matrix = cv2.getPerspectiveTransform(vertices, output_corners)
         warped_image = cv2.warpPerspective(img.copy(), transformation_matrix, (output_width, output_height))
         x, y = warped_image.shape[:2]
         # name = warped_image[x//16:x//7,y//12:y*33//40]
@@ -82,15 +95,16 @@ for c in contours:
         # warped_image = cv2.resize(warped_image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
         text = pt.image_to_string(name, lang='ita')
         print(text)
-        cv2.imshow(f"name of {count}", name)
+        cv2.imshow(f"name of {count}", cv2.resize(warped_image, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
 
     else:
-        cv2.drawContours(im2, [c], -1, (0,0,255), 8)
-        print("smt wrongobongo")
+        # cv2.drawContours(im2, [c], -1, (0,0,255), 8)
+        # print("smt wrongobongo")
         failed += 1
 
-im2 = cv2.resize(im2, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
-cv2.imshow("contours", im2)
+
+cv2.imshow("contours", cv2.resize(im2, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC))
+print(f"found {count} cards and {failed} failed")
 # im2 = img.copy()
 # lines = cv2.HoughLines(edges, 1, np.pi/180, 100)
 # for line in lines:
